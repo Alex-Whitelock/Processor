2f8206d84596886b1c0e429dcdcc2a4531ed92b4
daniel-clyde <daniel.clyde@gmail.com>
2015-10-24 12:33:05 -0600

    ProgCount and SignExtend SURE
    this is teh second time committing this, but this is the sure-fire one.

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/u0607599/Documents/GitHub/Processor/Test_Prog_Count_isim_beh.exe" -prj "C:/Users/u0607599/Documents/GitHub/Processor/Test_Prog_Count_beh.prj" "work.Test_Prog_Count" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/u0607599/Documents/GitHub/Processor/Sign_Test_isim_beh.exe" -prj "C:/Users/u0607599/Documents/GitHub/Processor/Sign_Test_beh.prj" "work.Sign_Test" "work.glbl" 
